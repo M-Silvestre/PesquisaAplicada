1a3c13db14771329507958ecb5ccbfb9a110fc29
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-19 13:26:09 -0300

    Create Script_Setores.R

---
+++ Script_Setores.R
@@ -0,0 +1,15 @@
+#Gráfico de Setores: Notas da primeira prova de Cálculo 2
+
+#Dados
+dados<-c(4.0, 3.0, 4.0, 0.0, 0.0, 2.0, 3.0, 1.0, 3.0, 3.0, 4.0, 0.0,
+         4.0, 3.0, 5.0, 1.0, 4.0, 3.0, 2.0, 4.0, 3.0, 4.0, 2.0, 1.0,
+         2.0, 4.0, 2.0, 3.0, 2.0, 4.0, 2.0, 2.0, 0.0, 5.0, 2.0, 4.0,
+         2.0, 0.0, 4.0, 3.0, 5.0, 4.0, 3.0, 0.0, 1.0, 4.0, 0.0, 5.0,
+         2.0, 1.0, 3.0, 2.0, 3.0, 3.0, 4.0, 4.0, 4.0, 4.0, 2.0, 5.0)
+
+#Dados: Quem tirou nota boa, quem tirou nota baixa (não nula) e quem faltou/tirou zero
+dados_set<-c(sum(dados > 3.5), sum(dados < 3.5) - sum(dados == 0.0), sum(dados == 0.0))
+
+pie(dados_set, main = "Notas da primeira avaliação de Cálculo 2",
+    labels = c("Notas acima de 70%", "Notas entre 0 e 70%", "Notas zero/Faltas"),
+    sub = "Fonte: SIGAA")
