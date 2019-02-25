630e10dfcdaee430428b20050bd6f62b91444eb2
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-02-25 16:38:56 -0300

    Update and rename Script_Histograma_Ogiva.R to Trabalho1/Script_Hist_Poligon_Ogiva.R

--- Trabalho1/Script_Hist_Poligon_Ogiva.R
+++ Trabalho1/Script_Hist_Poligon_Ogiva.R
@@ -19,6 +19,7 @@ classes <- round(1 + 3.3 * log10(length(idade)), 0)
 #                 from: Ponto inicial da sequência
 #                 to: Ponto final da sequência
 #                 length.out: quantos pontos na sequência (número de classes + 1)
+
 interv<-seq(from = 18.0, to = 27.0, length.out = (classes + 1))
 
 #Alguns argumentos do comando hist(): 
@@ -28,7 +29,7 @@ interv<-seq(from = 18.0, to = 27.0, length.out = (classes + 1))
 #                  xlab: Texto do eixo x
 #                  names: Nomes das colunas
 #                  break: breakpoints do histograma (limites das classes)
-#                  right: se os intervalos são fechados direita
+#                  right: se os intervalos são fechados à direita
 
 titulo<-paste(strwrap(
   'Idades dos corredores da Maratona Paraibana de 2015',
@@ -40,20 +41,49 @@ hist(idade, breaks = interv, main = titulo, xlim = range(idade),
 
 #Gerando o eixo x
 axis(1, at = c(18, 19.5, 21, 22.5, 24,25.5, 27), 
-     lab=c("18","19.5", "21", "22.5", "24", "25.5", "27"))
+     lab=c("18","", "21", "", "24", "", "27"))
 
 #Gerando o eixo y
 axis(2, at = c(0,2,4,6,8,10,12), labels = c("0","","4","","8","","12"))
 
+# Fechando o gráfico
+box()
+
+
+
+#Polígono de Frequência: Mesmo Banco que o Histograma
+
+#Criar um objeto hist()
+histograma<-hist(idade, breaks = interv, main = titulo, xlim = range(idade),
+     sub = "Fonte: Dados fictícios", right = FALSE, include.lowest = TRUE,
+     xlab = "Idade (anos)", ylab = "Frequência", col = "gray", axes = FALSE)
+
+#Cria-se o polígono após o histograma. Argumentos do comando polygon.freq():
+#             <primeiro argumento>: objeto historgama ao qual o polígono
+#                                   estará associado
+#             frequency: freqência a ser usada (1 - acumulada, 2 - relativa)
+#             col: cor
+polygon.freq(histograma, frequency=1,col="red")
+
+#OBS: O comando polygon.freq() faz parte do pacote "agricolae"
+
+#Gerando o eixo x
+axis(1, at = c(18, 19.5, 21, 22.5, 24,25.5, 27), 
+     lab=c("18","", "21", "", "24", "", "27"))
+
+#Gerando o eixo y
+axis(2, at = c(0,2,4,6,8,10,12), labels = c("0","","4","","8","","12"))
+
+# Fechando o gráfico
+box()
+
+
+
 #Ogiva de Galton ou Diagrama de Frequência Acumulada: Mesmo Banco que o Histograma
 
-#Gerando o Título
-titulo_ogiva<-paste(strwrap(
-  'Idades dos corredores da Maratona Paraibana de 2015',
-  width = 50),collapse = "\n")
-
-#Gerando uma matriz de vetores de um elemento da classe e a frequencia acumulada
-freq_idade<-c(rep(18,12), rep(20,16), rep(22,24), rep(23,27), rep(25,28), rep(26,30))
+#Gerando uma matriz de vetores de um elemento da classe e a frequência acumulada
+freq_idade<-c(rep(18.75, 12), rep(20.25, 16), rep(21.75, 24),
+  rep(23.25, 27), rep(24.75, 28), rep(26.25, 30))
 
 #Gerando a Ogiva (sem eixos)
 hist(freq_idade, breaks = interv, main = titulo, xlim = range(idade),
